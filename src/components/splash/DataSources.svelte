22a0745836136635f54ca69d7eb5d8a360326984
Jono Cooper <jono@jonocooper.com>
2021-03-07 12:46:14 +1300

    Move Data Sources to Svelte

---
+++ src/components/splash/DataSources.svelte
@@ -0,0 +1,58 @@
+<div>
+  <h3>Data Sources</h3>
+  <p>
+    For an accessible version of this data, please email
+    <a href="mailto:info@stats.govt.nz">info@stats.govt.nz</a>.
+  </p>
+  <p>
+    This app is built with data from the 2018 Census. It primarily uses the
+    <a
+      href="https://datafinder.stats.govt.nz/data/category/census/2018/commuter-view/"
+      >commuter view dataset</a
+    >, as well as aggregates from the
+    <a
+      href="http://nzdotstat.stats.govt.nz/WBOS/Index.aspx?DataSetCode=TABLECODE8296"
+      >NZ.Stat repository</a
+    >. The code for this website is open source on
+    <a href="https://github.com/consindo/commuterview">GitHub</a>—contributions
+    are welcome!
+  </p>
+  <p>
+    App not loading? Make sure you’re using the latest version of
+    <a href="https://firefox.com">Firefox</a>,
+    <a href="https://www.microsoft.com/en-us/edge">Edge</a>, or
+    <a href="https://www.google.com/chrome/">Chrome</a>. If you’re still having
+    issues, reach out on
+    <a href="https://twitter.com/consindo">Twitter</a>.
+  </p>
+</div>
+<footer><a href="https://jono.nz">&copy; 2020 Jono Cooper</a></footer>
+
+<style>
+  div {
+    padding: 1rem 0 0.5rem;
+  }
+
+  p {
+    padding: 0 var(--sidebar-padding);
+    line-height: 1.35;
+  }
+
+  a {
+    color: #90caf9;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  footer {
+    padding: 0 var(--sidebar-padding) 3rem;
+  }
+  footer a {
+    color: #999;
+    text-decoration: none;
+    font-size: 0.85rem;
+  }
+</style>
--- src/components/splash/Splash.svelte
+++ src/components/splash/Splash.svelte
@@ -1,7 +1,9 @@
 <script>
   import Banner from './Banner.svelte'
   import Regions from './Regions.svelte'
+  import DataSources from './DataSources.svelte'
 </script>
 
 <Banner />
 <Regions />
+<DataSources />
