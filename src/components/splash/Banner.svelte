d56f9be4a08db99a1da74a93374f5fc921a76a03
Jono Cooper <jono@jonocooper.com>
2025-03-10 23:37:04 +1300

    chore: switch to avif

--- src/components/splash/Banner.svelte
+++ src/components/splash/Banner.svelte
@@ -36,7 +36,7 @@
       ),
       url('/static/css/splash.avif');
     background-size: cover;
-    background-position: 50% 100%;
+    background-position: 50% 75%;
     height: 315px;
     padding: 4rem 1.25rem;
     box-sizing: border-box;
@@ -48,7 +48,7 @@
         0deg,
         var(--surface-bg) 0%,
         rgba(0, 0, 0, 0.5) 30%,
-        rgba(0, 0, 0, 0.05) 100%
+        rgba(0, 0, 0, 0.05) 75%
       ),
       url('/static/css/splash-dark.avif');
   }
